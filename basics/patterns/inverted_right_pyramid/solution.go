a090dd54f8627d258be0b40010693126933ad2af
johnathikalam <johnathikalam3@gmail.com>
2025-07-03 17:58:46 +0530

    solved inverted_right_pyramid

---
+++ basics/patterns/inverted_right_pyramid/solution.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func pattern(n int) {
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i; j++ {
+			fmt.Printf("* ")
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func main() {
+	pattern(6)
+}
